441c3ba92bc3e5f6b6713cf7c38436718bb8c355
Leandro Sousa <leandro.sousa@PPM-SPO-14723.local>
2020-10-06 14:10:16 -0300

    feat(angular) example angular

---
+++ examples/ff-angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FFModule } from 'ff-module-pp';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ff-angular';
+
+  ffmodule = new FFModule(environment.featureFlagApi + '/flags/features');
+}
---
+++ examples/ff-angular/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  featureFlagApi: 'https://gateway.service.ppay.mr/flags/features'
+};
